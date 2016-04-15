c5e89321951d1cfe015dd605e30c8eea9a735913
LM-G <louismarie.guillemot@gmail.com>
2016-04-15 17:52:06 +0200

    ajout backend / front end
    ajout jshint rules
    ajout express

---
+++ server/tuto-module.js
@@ -0,0 +1,11 @@
+"use strict";
+var direBonjour = function () {
+  console.log('Bonjour !');
+};
+
+var direByeBye = function () {
+  console.log('Bye bye !');
+};
+
+exports.direBonjour = direBonjour;
+exports.direByeBye = direByeBye;
